33fb6c2a93e0fe6ebb921512e54b4b0f5f4e2b85
Gaetan <gaetan.marchale@hotmail.com>
2023-08-10 17:15:11 +0200

    Start init_data

--- src/main.c
+++ src/main.c
@@ -1,14 +1,25 @@
 #include "../include/philosophers.h"
 
+int init_data(int argc, char **argv, t_data *data)
+{
+	data->num_p = ft_atoi(argv[1]);
+	data->time_d = ft_atoi(argv[2]);
+	data->time_e = ft_atoi(argv[3]);
+	data->time_s = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->num_e = ft_atoi(argv[5]);
+	else
+		data->num_e = 0;
+	return (0);
+}
+
 int main(int argc, char **argv)
 {
-	t_philo	*philo;
+	t_data	data;
 
-	philo = NULL;
 	if (parser(argc, argv))
 		return (1);
-	//if (init(argc, argv, &philo))
-	//	return (2);
-	printf("--->Fin de la main\n");
+	init_data(argc, argv, &data);
+	printf("---Fin de la main---\n");
 	return (0);
 }
